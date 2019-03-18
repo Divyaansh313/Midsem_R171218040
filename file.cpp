24a4f70ec212777eebfe1438a8cd2e08527bab48
Divyaansh Jain <divyaansh03anu@gmail.com>
2019-03-18 10:41:56 +0530

    made a prog with use of increment operator without comments or statements

--- file.cpp
+++ file.cpp
@@ -2,3 +2,16 @@
 using namespace std;
  
 int main()
+  {
+   int a = 1;
+   int c ;
+
+   c = a++;
+   cout << "Firstly Value of a++ is :" << c << endl ;
+
+   cout << "Secondly Value of a is :" << a << endl ;
+
+   c = ++a;
+   cout << "Thirdly Value of ++a is  :" << c << endl ;
+   return 0;
+   }
