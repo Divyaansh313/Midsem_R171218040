daccba1cf7217d479563b7868f7a5139a2e9b5b2
Divyaansh Jain <divyaansh03anu@gmail.com>
2019-03-18 10:44:06 +0530

    added comments or statements

--- file.cpp
+++ file.cpp
@@ -7,10 +7,13 @@ int main()
    int c ;
 
    c = a++;
+   // Value of a will not be increased before assignment.
    cout << "Firstly Value of a++ is :" << c << endl ;
-
+   
+   //After expression value of a is increased.
    cout << "Secondly Value of a is :" << a << endl ;
-
+   
+   //Value of a will be increased before assignment.
    c = ++a;
    cout << "Thirdly Value of ++a is  :" << c << endl ;
    return 0;
